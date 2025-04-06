74d7b10e71b221ae725acbcf25483dfb77c144d0
Isabella Gardner <75781502+bellavg@users.noreply.github.com>
2025-04-06 20:33:00 +0200

    fixed script typo

--- continue.sh
+++ continue.sh
@@ -30,7 +30,7 @@ CONFIG_FILE=${1:-"configs/config_aig_base.yaml"}
 echo "Using config file: $CONFIG_FILE"
 echo "Output directory: $OUTPUT_DIR"
 
-srun pyton -u --config_file configs/config_aig_base.yaml --restore new_runs/graphrnn_11003135/base_checkpoints/checkpoint-10000.pth --save_dir new_runs/graphrnn_11003135_continued
+srun python -u --config_file configs/config_aig_base.yaml --restore new_runs/graphrnn_11003135/base_checkpoints/checkpoint-10000.pth --save_dir new_runs/graphrnn_11003135_continued
 
 # Print job completion message
 echo "Job finished at $(date)"
