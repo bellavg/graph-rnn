039efedc4a902a438ee81676cc084f9b1a473c0e
Isabella Gardner <75781502+bellavg@users.noreply.github.com>
2025-04-14 16:29:31 +0200

    fixing import issue

--- scripts_and_slurm_logs/all_test_gen.sh
+++ scripts_and_slurm_logs/all_test_gen.sh
@@ -45,7 +45,10 @@ module load Anaconda3/2024.06-1 # Or your Anaconda/Python module
 echo "Activating Conda environment..."
 source activate aig-rnn         # <<< Make sure this conda env name is correct
 
-conda install numpy=1.26.1 scipy=1.11.2 --force-reinstall
+conda remove numpy scipy
+pip uninstall numpy scipy
+conda install scipy=1.11.2
+conda install numpy
 
 # --- Check if Python script exists ---
 if [ ! -f "$PYTHON_SCRIPT" ]; then
